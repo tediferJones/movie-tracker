b5b8f70e9cc22bdf28dbafc5a2ede459e6662578
ted <theo_drz@live.com>
2023-05-06 20:01:58 -0400

    user auth still doesnt work, ya know what would be 100x times easier?  Building an API from scratch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,37 @@
+'use client';
+import { signIn } from 'next-auth/react';
+
 export default function Login() {
-  // function formHandler(e: any) {
-  //   console.log(e)
-  // }
+  async function formHandler(e: any) {
+    e.preventDefault();
+    console.log(e)
+    console.log(e.target.username.value)
+    console.log(e.target.password.value)
+
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+
+    const result = await signIn('Credentials', { username, password , redirect: false })
+    console.log(result)
+    // const result = { username, password, error: undefined }
+    
+    if (result?.error) {
+      console.log('WRONG')
+    } else {
+      console.log('WINNER')
+    }
+  }
 
   return (
     <div>
       <h1>Login Page</h1>
 
-      <form method='POST' action=''>
-        <label>Username</label>
-        <input className='border-8' type='text' />
+      <form method='POST' action='api/auth/signin'>
+        <label htmlFor='username'>Username</label>
+        <input name='username' className='border-8' type='username' />
 
-        <label>Password</label>
-        <input className='border-8' type='password' />
+        <label htmlFor='password'>Password</label>
+        <input name='password' className='border-8' type='password' />
 
         <button>Login</button>
       </form>

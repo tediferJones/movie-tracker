158a8d70867c592618260a62198fbecfa92ae5c2
ted <theo_drz@live.com>
2023-05-21 21:27:29 -0400

    added more tables, started implementing add to DB functions when a new movie is looked up

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -2,13 +2,41 @@ import { rawMovieInfo, cleanMovieInfo, ratingObj } from '@/types';
 // import { auth } from '@clerk/nextjs';
 import { v4 as uuidv4 } from 'uuid';
 import AddToMyList from '@/components/AddToMyList';
+import prisma from '@/client';
 
 export default async function Movie({ params }: { params: any }) {
   // Search for param ID in DB before fetching from omdbapi
   // const { userId } = auth();
+  // Whats the process we need each movie page to go through?
+  //   - First check if the movie exists in the DB
+  //   - if it does, display those results
+  //   - if it does NOT, fetch it from omdb and add it to our DB, then display that result
+  //        - add the date added field to the cleanMovieInfo obj, add it to the types file too
+  // What API routes do we need?
+  //   - GET returns array of single movie obj from DB or empty array
+  //        /api/movies?{imdbId}
+  //   - POST add movie data to DB
+  //        /api/movies body = cleanMovieInfo obj
+
+  // Check if movie already exists in our DB, otherwise fetch from omdbapi and add to our DB
+  // STEP 1: WRITE THE API ROUTES
+  let dataV2: cleanMovieInfo;
+  const dbResult: cleanMovieInfo[] = await prisma.$queryRaw
+      `SELECT * FROM movies WHERE imdbId = ${params.id}`
+  console.log(dbResult)
+  if (dbResult.length === 1) {
+    dataV2 = dbResult[0];
+  } else {
+    const omdbResult = await fetch(`https://www.omdbapi.com/?apikey=8e1df54b&i=${params.id}`).then((res: any) => res.json());
+    const cleanOmdbResult = cleanUpMovieInfo(omdbResult);
+    console.log(cleanOmdbResult);
+    // prisma.$queryRaw`INSERT INTO movies ()`
+  }
+
   const res = await fetch(`https://www.omdbapi.com/?apikey=8e1df54b&i=${params.id}`)
   const rawData: rawMovieInfo = await res.json();
   const data: cleanMovieInfo = cleanUpMovieInfo(rawData);
+  console.log('poopoodoodoo')
 
   function cleanUpMovieInfo(movieObj: rawMovieInfo): cleanMovieInfo {
     // Split these strings into arrays of individual names

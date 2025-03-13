e65767d3980be423950cbe8c7968e0972bce0203
ted <theo_drz@live.com>
2025-03-12 23:46:38 -0400

    created new api routes for modifying lists and their contents

--- src/app/api/users/[username]/lists/[listname]/route.ts
+++ src/app/api/users/[username]/lists/[listname]/route.ts
@@ -1,11 +1,14 @@
 import { db } from '@/drizzle/db';
-import { lists } from '@/drizzle/schema';
+import { listnames, lists } from '@/drizzle/schema';
 import { getManyExistingMedia } from '@/lib/getManyExistingMedia';
+import { isValid } from '@/lib/inputValidation';
+import { currentUser } from '@clerk/nextjs';
 import { and, eq } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
 type Params = { username: string, listname: string }
 
+// IGNORE THIS, refer to src/app/api/users/[username]/lists/route.ts
 // /api/users/[username]/lists
 //  - GET: Get all listnames and default listname
 //
@@ -46,15 +49,98 @@ export async function GET(req: Request, { params }: { params: Params }) {
   return NextResponse.json(listData)
 }
 
-export async function POST() {
-  // add record to listname
+export async function POST(req: Request, { params }: { params: Params }) {
+  // create new list
+  const { username, listname } = params
+
+  const user = await currentUser();
+  if (!user?.username || user.username !== username) {
+    return NextResponse.json('Unauthorized', { status: 401 })
+  }
+
+  try {
+    const alreadyExists = await db.select().from(listnames).where(
+      and(
+        eq(listnames.username, username),
+        eq(listnames.listname, listname),
+      )
+    ).get()
+    if (alreadyExists) {
+      return NextResponse.json('Listname already exists', { status: 409 })
+    }
+
+    await db.insert(listnames).values({ username, listname, defaultList: false })
+  } catch {
+    return NextResponse.json('Failed to process request, database error', { status: 500 })
+  }
+
+  return new NextResponse
 }
 
 // use PUT /users/[username]/lists/[listname]/default to set default lists
-export async function PUT() {
-  // list bump feature?
+export async function PUT(req: Request, { params }: { params: Params }) {
+  // update listname
+  const { username, listname } = params
+  const newListname = await req.json()
+  
+  if (!newListname) {
+    return NextResponse.json('Bad Request', { status: 400 })
+  }
+
+  const user = await currentUser();
+  if (!user?.username || user.username !== username) {
+    return NextResponse.json('Unauthorized', { status: 401 })
+  }
+
+  const valid = isValid({ listname })
+  if (!valid) return NextResponse.json('inputs are not valid', { status: 422 })
+
+  try {
+    await db.update(listnames).set({ listname: newListname }).where(
+      and(
+        eq(listnames.username, username),
+        eq(listnames.listname, listname),
+      )
+    )
+
+    await db.update(lists).set({ listname: newListname }).where(
+      and(
+        eq(lists.username, username),
+        eq(lists.listname, listname),
+      )
+    )
+  } catch {
+    return NextResponse.json('Failed to process request, database error', { status: 500 })
+  }
+
+  return new NextResponse
 }
 
-export async function DELETE() {
+export async function DELETE(req: Request, { params }: { params: Params }) {
   // delete entire list
+  const { username, listname } = params
+
+  const user = await currentUser();
+  if (!user?.username || user.username !== username) {
+    return NextResponse.json('Unauthorized', { status: 401 })
+  }
+
+  try {
+    await db.delete(listnames).where(
+      and(
+        eq(listnames.username, username),
+        eq(listnames.listname, listname),
+      )
+    )
+    await db.delete(lists).where(
+      and(
+        eq(lists.username, username),
+        eq(lists.listname, listname),
+      )
+    )
+  } catch {
+    return NextResponse.json('Failed to process request, database error', { status: 500 })
+  }
+
+  return new NextResponse
 }
--- src/app/api/users/[username]/lists/route.ts
+++ src/app/api/users/[username]/lists/route.ts
@@ -5,6 +5,21 @@ import { NextResponse } from 'next/server';
 
 type Params = { username: string }
 
+// /api/users/[username]/lists
+// GET: get all listnames and default list
+// POST/PUT: change default listname
+//
+// /api/users/[username]/lists/[listname]
+// [ DONE ] GET: get list contents and all associated media data
+// [ DONE ] POST: create new list with listname
+// [ DONE ] PUT: change listname (new listname in URL or body)
+// [ DONE ] DELETE: remove list (remove from listnames table and make sure all list data for listname is removed from lists table)
+//
+// /api/users/[username]/lists/[listname]/[imdbId]
+// [ DONE ] POST: add imdbId to list
+// PUT: bump imdbId in list
+// [ DONE ] DELETE: remove imdbId from list
+
 export async function GET(req: Request, { params }: { params: Params }) {
   const { username } = params;
 
@@ -19,12 +34,14 @@ export async function GET(req: Request, { params }: { params: Params }) {
   });
 }
 
-export async function POST(req: Request, { params }: { params: Params }) {
-  // create new lists here
-  // new listname should be in req body
-  // const { username } = params;
+// export async function POST(req: Request, { params }: { params: Params }) {
+//   // create new lists here
+//   // new listname should be in req body
+//   // const { username } = params;
+// 
+//   // const { listname } = await req.json()
+// }
 
-  // const { listname } = await req.json()
-}
-
-// no PUT or DELETE methods here
+// export async function PUT(req: Request, { params }: { params: Params }) {
+// 
+// }
